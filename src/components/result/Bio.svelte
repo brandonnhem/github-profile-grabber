86820ed907770692a7fb0f7ceb672198e5bdd8a1
brandonnhem <brandonnhem@gmail.com>
2021-09-09 14:31:21 -0700

    finished mobile view for bio section

---
+++ src/components/result/Bio.svelte
@@ -0,0 +1,53 @@
+<div class="bio">
+    <div class="bio-header">
+        <div class="profile-pic">
+            <img src="https://avatars.githubusercontent.com/u/583231?v=4" alt="profile">
+        </div>
+        <div class="profile-info">
+            <h3>USERNAME</h3>
+            <h4><a href="/">@HANDLE</a></h4>
+            <h4>Joined 9 Sep 2021</h4>
+        </div>
+    </div>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, facere magni maiores totam, ea repellat eveniet, corrupti tempora.</p>
+</div>
+
+<style>
+    p {
+        color:hsl(217, 35%, 45%);
+    }
+
+    .bio-header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    img {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+    }
+
+    .profile-info {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 5vw;
+    }
+
+    h3 {
+        font-weight: 700;
+        margin: 0;
+    }
+
+    h4 {
+        margin: 0;
+        color: hsl(217, 20%, 51%);
+    }
+
+    a {
+        color: hsl(212, 100%, 50%);
+        text-decoration: none;
+    }
+</style>
