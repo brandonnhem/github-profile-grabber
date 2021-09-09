f04a7e5612b9dc58c94bb418ed8a5a9fcfb02a52
brandonnhem <brandonnhem@gmail.com>
2021-09-09 14:31:37 -0700

    finished mobile view

---
+++ src/components/result/Stats.svelte
@@ -0,0 +1,47 @@
+<div class="stats">
+    <div class="stats-col">
+        <p>Repos</p>
+        <h3>8</h3>
+    </div>
+    <div class="stats-col">
+        <p>Followers</p>
+        <h3>3938</h3>
+    </div>
+    <div class="stats-col">
+        <p>Following</p>
+        <h3>9</h3>
+    </div>
+</div>
+
+<style>
+    .stats {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-evenly;
+        background-color: hsl(227, 100%, 98%);
+        border-radius: 15px;
+    }
+
+    .stats-col {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0;
+    }
+
+    p {
+        font-size: 11px;
+        color:hsl(217, 35%, 45%);
+        padding-top: 1rem;
+    }
+
+    h3 {
+        font-weight: 700;
+        padding-bottom: 1rem;
+    }
+
+    p, h3 {
+        margin: 0;
+    }
+</style>
