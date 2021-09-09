3643a10643ee3ff1b1521dc68bb4282fe4182e3e
brandonnhem <brandonnhem@gmail.com>
2021-09-09 14:48:43 -0700

    wrapper component added

---
+++ src/components/result/Result.svelte
@@ -0,0 +1,41 @@
+<script>
+    import Bio from './Bio.svelte';
+    import Stats from './Stats.svelte';
+    import Links from './Links.svelte';
+    import { Octokit } from "@octokit/rest";
+
+    const octokit = new Octokit({
+        auth: API_KEY,
+        userAgent: 'devfinder v.1',
+    });
+
+
+    // TODO: create GET method
+
+    // myAsyncMethod();
+
+    // async function myAsyncMethod() {
+    //     const result = await octokit.request('GET /users/{username}', {
+    //         username: 'brandonnhem'
+    //     });
+    //     console.log(result);
+    // }
+</script>
+
+<section class="result">
+    <Bio />
+    <Stats />
+    <Links />
+</section>
+
+<style>
+    .result {
+        background-color: white;
+        border-radius: 15px;
+        padding: 1.5rem;
+        margin-top: 3vh;
+        box-shadow: 0 4px 4px 0 rgba(0,0,0,0.25);
+        -webkit-box-shadow: 0 4px 4px 0 rgba(0,0,0,0.25);
+        -moz-box-shadow: 0 4px 4px 0 rgba(0,0,0,0.25);
+    }
+</style>
