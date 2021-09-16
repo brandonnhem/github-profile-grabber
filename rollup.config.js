2c3c1fb6707653ab09aac51bd40dbebe7a06bbe1
brandonnhem <brandonnhem@gmail.com>
2021-09-15 17:23:18 -0700

    removed mention of dotenv and replace

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-import replace from '@rollup/plugin-replace';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -41,10 +37,6 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
-		replace({
-			preventAssignment: true,
-            API_KEY: JSON.stringify(process.env.API_KEY)
-        }),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
